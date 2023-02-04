9911960f00031da4f20cd3c0fa9bcfbc8b3c4026
V1ck105 <123528985+V1ck105@users.noreply.github.com>
2023-02-03 19:16:05 -0600

    Primer Commit
    Primer commit del ejercicio.

---
+++ Tarea1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Tarea1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IngreseTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ResultadoTextBox = new System.Windows.Forms.TextBox();
+            this.CalcularButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(199, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(498, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Determine Si es Par o impar Positivo O negativo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(145, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese un numero:";
+            // 
+            // IngreseTextBox
+            // 
+            this.IngreseTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IngreseTextBox.ForeColor = System.Drawing.Color.Red;
+            this.IngreseTextBox.Location = new System.Drawing.Point(336, 69);
+            this.IngreseTextBox.Name = "IngreseTextBox";
+            this.IngreseTextBox.Size = new System.Drawing.Size(280, 24);
+            this.IngreseTextBox.TabIndex = 2;
+            this.IngreseTextBox.Text = "4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(161, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resultado:";
+            // 
+            // ResultadoTextBox
+            // 
+            this.ResultadoTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultadoTextBox.ForeColor = System.Drawing.Color.Red;
+            this.ResultadoTextBox.Location = new System.Drawing.Point(336, 114);
+            this.ResultadoTextBox.Name = "ResultadoTextBox";
+            this.ResultadoTextBox.Size = new System.Drawing.Size(280, 24);
+            this.ResultadoTextBox.TabIndex = 4;
+            // 
+            // CalcularButton
+            // 
+            this.CalcularButton.Font = new System.Drawing.Font("Microsoft JhengHei", 10.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalcularButton.ForeColor = System.Drawing.Color.Blue;
+            this.CalcularButton.Location = new System.Drawing.Point(413, 163);
+            this.CalcularButton.Name = "CalcularButton";
+            this.CalcularButton.Size = new System.Drawing.Size(96, 30);
+            this.CalcularButton.TabIndex = 5;
+            this.CalcularButton.Text = "Calcular";
+            this.CalcularButton.UseVisualStyleBackColor = true;
+            this.CalcularButton.Click += new System.EventHandler(this.CalcularButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CalcularButton);
+            this.Controls.Add(this.ResultadoTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.IngreseTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IngreseTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ResultadoTextBox;
+        private System.Windows.Forms.Button CalcularButton;
+    }
+}
+
---
+++ Tarea1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularButton_Click(object sender, EventArgs e)
+        {
+            int num;
+            num = int.Parse(IngreseTextBox.Text);
+            //Esta funcion nos ayudara a determinar si el numero es par o impar positivo o negativo//
+            if (num % 2 == 0)
+            {
+                if (num >= 0) 
+                {
+
+                    ResultadoTextBox.Text = "El Numero es Par y es Positivo";
+                }
+               else
+                {
+                    ResultadoTextBox.Text = "El Numero es Par y es Negativo";
+                }
+
+            }
+            else
+
+            if (num >= 0)
+            {
+                ResultadoTextBox.Text = "El numero es Impar y es positivo";
+            }
+            else
+            {
+                ResultadoTextBox.Text = "El numero es Impar y es Negativo";
+            }
+        }
+    }
+}
